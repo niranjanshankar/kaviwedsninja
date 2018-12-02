684918820d7c01658b517c397b050f4638deca77
Ninja <niranjans241992@gmail.com>
2018-12-02 19:47:41 +0000

    favicon added, RSVP changed to wishes

--- script.js
+++ script.js
@@ -7,21 +7,18 @@ $(document).ready(function(){
         $("#daysText").html("Days")
         $("#numberOfDays").html(days);
         $("#rsvpForm").show();
-        $("#rsvpExpiry").hide();
+        $("#wishesSent").hide();
     }
     else{
         $("#numberOfDays").html("IT'S OVER");
-        $("#rsvpForm").hide();
-        $("#rsvpExpiry").show();
+        $("#rsvpForm").show();
+        $("#wishesSent").hide();
     }
     
 
-    // $('#myTab').tabCollapse();
-    // $('#myTab').on('shown-accordion.bs.tabcollapse', function(){
-    //     alert('accordion is shown now!');
-    // });
-
 });
+
+// https://script.google.com/macros/s/AKfycbxHN5SNaJHHp98EPKf-qLgKfhO3ysKd5q_9WlPAnHT12WDLJGF5/exec
 
 //to consider day light savings also
 function treatAsUTC(result) {
@@ -33,4 +30,44 @@ function treatAsUTC(result) {
 function daysBetween(startDate, endDate) {
     var millisecondsPerDay = 24 * 60 * 60 * 1000;
     return (treatAsUTC(endDate) - treatAsUTC(startDate)) / millisecondsPerDay;
+}
+
+function sendWish(){
+    // event.preventDefault();
+    // event.stopPropagation();
+
+    str = $("#wishContent").val().trim();
+    person = $("#name").val().trim();
+
+    if(str.length > 0 && person.length > 0){
+
+        $('#wishBtn').html('Sending Wish...');
+        $('#wishBtn').attr('disabled', true);
+
+        var url = "https://script.google.com/macros/s/AKfycbxHN5SNaJHHp98EPKf-qLgKfhO3ysKd5q_9WlPAnHT12WDLJGF5/exec";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url+'?content='+str+'&person='+person, true);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                $("#wishesSent").show();
+                $("#sendWishes").hide();
+                $("#wishContent").val('');
+                $("#name").val('');
+
+                $('#wishBtn').html('Send My Wish');
+                $('#wishBtn').attr('disabled', false);
+            }
+            else {
+                alert('I am sorry, we cannot send your wish now, please try again later!');
+            }
+        };
+        xhr.send();
+
+    }
+}
+
+function showWishHolder(){
+  $("#wishesSent").hide();
+    $("#sendWishes").show();   
 }
